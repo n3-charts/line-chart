b5f8850c2097c6db4f5ff99fc5497a37431d62bd
lorem--ipsum <vaporzub@gmail.com>
2013-05-09 18:01:25 +0200

    Added wonderful sinus function to the demo app

--- demo-app.js
+++ demo-app.js
@@ -12,6 +12,28 @@ angular.module('myApp', [
   }])
 
 .controller('DemoCtrl', ['$scope', function($scope) {
+  var colors = d3.scale.category10()
+  
+  $scope.sinusodize = function() {
+    $scope.options = {series: []};
+    
+    $scope.data = [];
+    for (var i = 0; i < 5; i++) {
+      $scope.options.series.push({
+        y: 'series_' + i,
+        color: colors(i)
+      });
+      
+      for (var j = 0; j < 50; j++) {
+        var row = $scope.data[j] || {x: j};
+        
+        row['series_' + i] = (Math.sin(j/5))*(5*i);
+        
+        $scope.data[j] = row;
+      }
+    }
+  }
+  
   $scope.randomize = function() {
     $scope.generate(
       Math.random()*10,
@@ -19,10 +41,8 @@ angular.module('myApp', [
     );
   }
   
-  var colors = d3.scale.category10()
   
   $scope.generate = function(seriesCount, rowCount) {
-    
     $scope.options = {series: []};
     
     $scope.data = [];
@@ -42,5 +62,5 @@ angular.module('myApp', [
     }
   }
   
-  $scope.generate(5, 50)
+  $scope.sinusodize();
 }])
