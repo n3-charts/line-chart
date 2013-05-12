a80a73cf3f5041f06a6b3bfba8d0a588c5834445
lorem--ipsum <vaporzub@gmail.com>
2013-05-12 08:10:04 +0200

    Almost done with second axis, only the tooltip is missing

--- line-chart.spec.js
+++ line-chart.spec.js
@@ -110,10 +110,13 @@ describe('n3-linechart', function() {
           {x: 3, value: 16, foo: 0}, {x: 4, value: 23, foo: -3}, {x: 5, value: 42, foo: -4}
         ];
         
-        scope.options = {series: [
-          {axis: 'y', y: 'value', color: '#4682b4'},
-          {axis: 'y2', y: 'foo', color: 'whatever'}
-        ]}
+        scope.options = {
+          showArea: true,
+          series: [
+            {axis: 'y', y: 'value', color: '#4682b4'},
+            {axis: 'y2', y: 'foo', color: 'steelblue'}
+          ]
+        }
       });
     })
     
@@ -139,45 +142,99 @@ describe('n3-linechart', function() {
       expect(ticks[10].textContent).toBe('16');
     });
     
-    // it('should draw dots', function() {
-    //   var svgGroup = elm.find('svg').children()[0];
-    //   var content = svgGroup.childNodes[4];
-    //   var dotsGroup = content.childNodes[1];
-    //   expect(dotsGroup.nodeName).toBe('g');
-      
-    //   var dots = dotsGroup.childNodes;
-    //   expect(dots.length).toBe(6);
-      
-    //   var expectedCoordinates = [
-    //     {x: '0', y: '414'},
-    //     {x: '161', y: '378'},
-    //     {x: '322', y: '315'},
-    //     {x: '483', y: '306'},
-    //     {x: '644', y: '243'},
-    //     {x: '805', y: '72'}
-    //   ];
-      
-    //   for (var i = 0; i < dots.length; i++) {
-    //     expect(dots[i].nodeName).toBe('circle');
-    //     expect(dots[i].getAttribute('cx')).toBe(expectedCoordinates[i].x);
-    //     expect(dots[i].getAttribute('cy')).toBe(expectedCoordinates[i].y);
-    //   }
-    // });
-    
-    it('should draw a line', function() {
+    it('should draw two lines', function() {
       var content = elm.find('svg').children()[0].childNodes[6];
       
-      var leftLinePath = content.childNodes[0].childNodes[0];
+      var leftLinePath = content.childNodes[2].childNodes[0];
       expect(leftLinePath.getAttribute('class')).toBe('line');
       expect(leftLinePath.getAttribute('d'))
         .toBe('M0,414L161,378L322,315L483,306L644,243L805,72');
       
-      var rightLinePath = content.childNodes[1].childNodes[0];
+      var rightLinePath = content.childNodes[3].childNodes[0];
       expect(rightLinePath.getAttribute('class')).toBe('line');
       expect(rightLinePath.getAttribute('d'))
         .toBe('M0,415L161,0L322,398L483,381L644,433L805,450');
     });
+    
+    it('should draw y area', function() {
+      var content = elm.find('svg').children()[0].childNodes[6];
+      
+      var areaGroup = content.childNodes[0];
+      expect(areaGroup.getAttribute('class')).toBe('areaGroup');
+      expect(areaGroup.getAttribute('style').trim()).toBe('fill: #4682b4;');
+      
+      var areaPath = areaGroup.childNodes[0];
+      expect(areaPath.getAttribute('class')).toBe('area');
+      expect(areaPath.getAttribute('d'))
+        .toBe('M0,414L161,378L322,315L483,306L644,243L805,72L805,450L644,' +
+          '450L483,450L322,450L161,450L0,450Z');
+    });
+    
+    it('should draw y2 area', function() {
+      var content = elm.find('svg').children()[0].childNodes[6];
+      
+      var areaGroup = content.childNodes[1];
+      expect(areaGroup.getAttribute('class')).toBe('areaGroup');
+      expect(areaGroup.getAttribute('style').trim()).toBe('fill: #4682b4;');
+      
+      var areaPath = areaGroup.childNodes[0];
+      expect(areaPath.getAttribute('class')).toBe('area');
+      expect(areaPath.getAttribute('d'))
+        .toBe('M0,415L161,0L322,398L483,381L644,433L805,450L805,381L644,' +
+          '381L483,381L322,381L161,381L0,381Z');
+    });
+    
+    it('should draw y axis dots', function() {
+      var content = elm.find('svg').children()[0].childNodes[6];
+      
+      var leftDotsGroup = content.childNodes[4];
+      expect(leftDotsGroup.nodeName).toBe('g');
+      
+      var dots = leftDotsGroup.childNodes;
+      expect(dots.length).toBe(6);
+      
+      var expectedCoordinates = [
+        {x: '0', y: '414'},
+        {x: '161', y: '378'},
+        {x: '322', y: '315'},
+        {x: '483', y: '306'},
+        {x: '644', y: '243'},
+        {x: '805', y: '72'}
+      ];
+      
+      for (var i = 0; i < dots.length; i++) {
+        expect(dots[i].nodeName).toBe('circle');
+        expect(dots[i].getAttribute('cx')).toBe(expectedCoordinates[i].x);
+        expect(dots[i].getAttribute('cy')).toBe(expectedCoordinates[i].y);
+      }
+    });
+  
+    it('should draw y2 axis dots', function() {
+      var content = elm.find('svg').children()[0].childNodes[6];
+      
+      var leftDotsGroup = content.childNodes[5];
+      expect(leftDotsGroup.nodeName).toBe('g');
+      
+      var dots = leftDotsGroup.childNodes;
+      expect(dots.length).toBe(6);
+      
+      var expectedCoordinates = [
+        {x: '0', y: '415'},
+        {x: '161', y: '0'},
+        {x: '322', y: '398'},
+        {x: '483', y: '381'},
+        {x: '644', y: '433'},
+        {x: '805', y: '450'}
+      ];
+      
+      for (var i = 0; i < dots.length; i++) {
+        expect(dots[i].nodeName).toBe('circle');
+        expect(dots[i].getAttribute('cx')).toBe(expectedCoordinates[i].x);
+        expect(dots[i].getAttribute('cy')).toBe(expectedCoordinates[i].y);
+      }
+    });
   });
+  
   
   describe('tooltip', function() {
     beforeEach(function() {
