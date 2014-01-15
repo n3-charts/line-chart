81db5f74e7ec8a25e914ae56240ff8ce42c7cda0
lorem--ipsum <vaporzub@gmail.com>
2014-01-15 10:03:39 -0800

    Updated to latest libs and fixed tests

--- karma.conf.js
+++ karma.conf.js
@@ -1,69 +1,80 @@
 // Karma configuration
-// Generated on Mon May 06 2013 08:16:19 GMT+0200 (CEST)
+// Generated on Wed Jan 15 2014 08:34:10 GMT-0800 (PST)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
 
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+    // frameworks to use
+    frameworks: ['jasmine'],
 
 
-// list of files / patterns to load in the browser
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'bower_components/angular/angular.js',
-  'bower_components/angular-mocks/angular-mocks.js',
-  'bower_components/d3/d3.js',
-  'dist/line-chart.min.js',
-  '/tmp/line-chart.spec.js'
-];
+    // list of files / patterns to load in the browser
+    files: [
+      'bower_components/angular/angular.js',
+      'bower_components/angular-mocks/angular-mocks.js',
+      'bower_components/d3/d3.js',
+      'dist/line-chart.min.js',
+      '/tmp/line-chart.spec.js'
+    ],
 
-preprocessors = {
-  'dist/line-chart.min.js': 'coverage'
+
+    // list of files to exclude
+    exclude: [
+      
+    ],
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+    reporters: ['progress', 'coverage'],
+
+    preprocessors: {
+      'dist/line-chart.min.js': 'coverage'
+    },
+
+    coverageReporter: {
+      type : 'text-summary',
+      dir : 'coverage/'
+    },
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera (has to be installed with `npm install karma-opera-launcher`)
+    // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
+    // - PhantomJS
+    // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
+    browsers: ['PhantomJS'],
+
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
+
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false
+  });
 };
-
-// list of files to exclude
-exclude = [
-];
-
-
-coverageReporter = {
-  type : 'text-summary',
-  dir : 'coverage/'
-}
-
-// test results reporter to use
-// possible values: 'dots', 'progress', 'junit'
-reporters = ['dots', 'coverage'];
-
-
-// web server port
-port = 9876;
-
-
-// cli runner port
-runnerPort = 9100;
-
-
-// enable / disable colors in the output (reporters and logs)
-colors = true;
-
-
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
-
-
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = true;
-
-
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-// browsers = ['PhantomJS'];
-browsers = ['PhantomJS']//, 'Firefox']
