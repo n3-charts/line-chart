1cb79ad52d99e93a16a299d4e6e7c4b17e8a37bc
lorem--ipsum <vaporzub@gmail.com>
2014-05-04 16:44:45 +0200

    Improved tests page

--- scripts/tests.js
+++ scripts/tests.js
@@ -3,21 +3,13 @@ angular.module('tests', [])
 
 .filter('firstUp', function() {
   return function(value) {
-    if (!value) {
-      return ''
-    }
-
-    return value.charAt(0).toUpperCase() + value.slice(1);
+    return value ? value.charAt(0).toUpperCase() + value.slice(1) : '';
   };
 })
 
 .filter('round', function() {
   return function(value) {
-    if (!value) {
-      return ''
-    }
-
-    return parseInt(value, 10);
+    return value ? parseInt(value, 10) : '';
   };
 })
 
@@ -25,6 +17,36 @@ angular.module('tests', [])
   mixpanel.track("Tests");
   d3.json('data/test_results.json', function(error, result) {
     $scope.tests = result.results;
+    b = $scope.build = result.build;
+
+    if (b.is_a_pull_request !== null) {
+      $scope.description = "Pull request #" + b.is_a_pull_request + " (for merge into " + b.branch + ")";
+      $scope.link = "https://github.com/n3-charts/line-chart/pull/" + b.is_a_pull_request;
+    } else {
+      $scope.description = "Branch " + b.branch;
+      $scope.link = "https://github.com/n3-charts/line-chart/tree/" + b.branch;
+    }
+
     $scope.$apply();
   });
+
+  $scope.expand = function(test, event) {
+    if (event.altKey) {
+      $scope.tests.forEach(function(t) {
+        t.expanded = true;
+      });
+    } else {
+      test.expanded = true;
+    }
+  };
+
+  $scope.collapse = function(test, event) {
+    if (event.altKey) {
+      $scope.tests.forEach(function(t) {
+        t.expanded = false;
+      });
+    } else {
+      test.expanded = false;
+    }
+  };
 });
