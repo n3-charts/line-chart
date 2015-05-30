a366c20bb5c5c338610d4156eb145075529dc7e7
Christoph Koerner <christoph.koerner@gesim.ch>
2015-05-30 17:45:10 +0200

    Fixed Karma to run separatly

--- karma.conf.js
+++ karma.conf.js
@@ -2,6 +2,20 @@ module.exports = function(config) {
   var configuration = {
     basePath: '.',
     frameworks: ['mocha'],
+    files: [
+      'node_modules/expect.js/index.js',
+      'node_modules/angular/angular.js',
+      'node_modules/angular-mocks/angular-mocks.js',
+      'node_modules/d3/d3.js',
+      'build/utils/*.js',
+      'build/factories/*.js',
+      'build/linechart.js',
+      'build/app.js',
+      '.tmp/unit/**/*.js',
+      {pattern: 'build/**/*.map', included: false},
+      {pattern: '.tmp/**/*.map', included: false}
+
+    ],
     reporters: ['progress', 'coverage'],
     coverageReporter: {
       type : 'lcovonly',
@@ -11,7 +25,7 @@ module.exports = function(config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: false,
-    singleRun: true,
+    singleRun: false,
     browsers: ['Chrome'],
     customLaunchers: {
       Chrome_travis_ci: {
