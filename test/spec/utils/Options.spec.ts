6b3483aac2c8435df1c47c8ded3f923b8b6810e7
Christoph Koerner <christoph.koerner@gesim.ch>
2015-09-28 18:12:55 +0200

    Added some tests for Options.ts

--- test/spec/utils/Options.spec.ts
+++ test/spec/utils/Options.spec.ts
@@ -18,39 +18,9 @@ describe('n3Charts.Utils.Options', () => {
 
       expect(testing).to.equal(expected);
     });
-  });
 
-  describe('parse', () => {
-    beforeEach(() => {
+    it('should create an array as series property', () => {
       options = new Options();
-    });
-
-    it('should call parseSeries function with series property', () => {
-      var arg = { series: ['foo', 'bar'] };
-      var spy = sinon.spy(options, 'parseSeries');
-
-      options.parse(arg);
-
-      var testing = spy.calledWith(arg.series);
-      var expected = true;
-
-      expect(testing).to.equal(expected);
-    });
-
-    it('should call parseSeries function with default arguments', () => {
-      var arg = { foo: 'bar' };
-      var spy = sinon.spy(options, 'parseSeries');
-
-      options.parse(arg);
-
-      var testing = spy.calledWith(Options.DEFAULT.series);
-      var expected = true;
-
-      expect(testing).to.equal(expected);
-    });
-
-    it('should create an empty array as series property when called without arguments', () => {
-      options.parse();
 
       var testing = angular.isArray(options.series);
       var expected = true;
@@ -58,144 +28,79 @@ describe('n3Charts.Utils.Options', () => {
       expect(testing).to.equal(expected);
     });
 
-    it('should create an array of series as series property', () => {
-      var arg = { series: [{foo: 'bar'}] };
+    it('should create an object as margin property', () => {
+      options = new Options();
 
-      options.parse(arg);
-
-      var testing = options.series[0] instanceof SeriesOptions;
+      var testing = angular.isObject(options.margin);
       var expected = true;
 
       expect(testing).to.equal(expected);
     });
 
-    it('should call parseAxes function with axes property', () => {
-      var arg = { axes: { 'foo': 'bar' } };
-      var spy = sinon.spy(options, 'parseAxes');
-
-      options.parse(arg);
-
-      var testing = spy.calledWith(arg.axes);
-      var expected = true;
-
-      expect(testing).to.equal(expected);
-    });
-
-    it('should call parseAxes function with default arguments', () => {
-      var arg = { foo: 'bar' };
-      var spy = sinon.spy(options, 'parseAxes');
-
-      options.parse(arg);
-
-      var testing = spy.calledWith(Options.DEFAULT.axes);
-      var expected = true;
-
-      expect(testing).to.equal(expected);
-    });
-
-    it('should create an axes property when called without arguments', () => {
-      options.parse();
+    it('should create an object as axes property', () => {
+      options = new Options();
 
       var testing = angular.isObject(options.axes);
       var expected = true;
 
       expect(testing).to.equal(expected);
     });
-
-    it('should create an axes property with x axis options', () => {
-      options.parse();
-
-      var testing = options.axes[Axis.SIDE.X] instanceof AxisOptions;
-      var expected = true;
-
-      expect(testing).to.equal(expected);
-    });
-
-    it('should create an axes property with y axis options', () => {
-      options.parse();
-
-      var testing = options.axes[Axis.SIDE.Y] instanceof AxisOptions;
-      var expected = true;
-
-      expect(testing).to.equal(expected);
-    });
   });
 
-  describe('parseSeries', () => {
+  describe('getSanitizedOptions', () => {
     beforeEach(() => {
       options = new Options();
+    });
+
+    it('should create an array as series property', () => {
+      var opt = options.getSanitizedOptions();
+
+      var testing = angular.isArray(opt.series);
+      var expected = true;
+
+      expect(testing).to.equal(expected);
     });
 
     it('should throw a type error when series argument is not an array', () => {
-      var arg = { foo: 'bar' };
+      var arg = { series: {foo: 'bar'} };
 
       expect(() => {
-        options.parseSeries(arg);
+          options.getSanitizedOptions(arg);
       }).to.throwError();
     });
 
-    it('should call sanitizeSeries function with default arguments', () => {
-      var arg = [];
-      var spy = sinon.spy(options, 'sanitizeSeries');
+    it('should create an object as margin property', () => {
+      var opt = options.getSanitizedOptions();
 
-      options.parseSeries(arg);
-
-      var testing = spy.calledWith(Options.DEFAULT.series);
+      var testing = angular.isObject(opt.margin);
       var expected = true;
 
       expect(testing).to.equal(expected);
     });
 
-    it('should call sanitizeSeries function and merge arguments with default options', () => {
-      var arg = ['foo'];
-      var mergedArgsWithDefault = ['foo'];
-      var spy = sinon.spy(options, 'sanitizeSeries');
+    it('should throw a type error when margin argument is not an object', () => {
+      var arg = { margin: 'bar' };
 
-      options.parseSeries(arg);
+      expect(() => {
+          options.getSanitizedOptions(arg);
+      }).to.throwError();
+    });
 
-      var testing = spy.calledWith(mergedArgsWithDefault);
+    it('should create an object as axes property', () => {
+      var opt = options.getSanitizedOptions();
+
+      var testing = angular.isObject(opt.axes);
       var expected = true;
 
       expect(testing).to.equal(expected);
-    });
-  });
-
-  describe('parseAxes', () => {
-    beforeEach(() => {
-      options = new Options();
     });
 
     it('should throw a type error when axes argument is not an object', () => {
-      var arg = 'foo';
+      var arg = { axes: 'bar' };
 
       expect(() => {
-        options.parseAxes(arg);
+          options.getSanitizedOptions(arg);
       }).to.throwError();
-    });
-
-    it('should call sanitizeAxes function with default arguments', () => {
-      var arg = {};
-      var spy = sinon.spy(options, 'sanitizeAxes');
-
-      options.parseAxes(arg);
-
-      var testing = spy.calledWith(Options.DEFAULT.axes);
-      var expected = true;
-
-      expect(testing).to.equal(expected);
-    });
-
-    it('should call sanitizeAxes function and merge arguments with default options', () => {
-      var arg = {x: 'foo'};
-      var mergedArgsWithDefault = { x: 'foo', y: {} };
-      var spy = sinon.spy(options, 'sanitizeAxes');
-
-      options.parseAxes(arg);
-
-      var testing = spy.calledWith(mergedArgsWithDefault);
-      var expected = true;
-
-      expect(testing).to.equal(expected);
     });
   });
 
@@ -229,7 +134,7 @@ describe('n3Charts.Utils.Options', () => {
     });
 
     it('should return all series for type LINE', () => {
-      var testing = options.getSeriesByType(Options.SERIES_TYPES.LINE);
+      var testing = options.getSeriesByType(SeriesOptions.TYPE.LINE);
       var expected = [
       options.series[0],
       options.series[2]
@@ -239,7 +144,7 @@ describe('n3Charts.Utils.Options', () => {
     });
 
     it('should return all series for type AREA', () => {
-      var testing = options.getSeriesByType(Options.SERIES_TYPES.AREA);
+      var testing = options.getSeriesByType(SeriesOptions.TYPE.AREA);
       var expected = [
       options.series[1]
       ];
@@ -248,7 +153,7 @@ describe('n3Charts.Utils.Options', () => {
     });
 
     it('should return all series for type COLUMN', () => {
-      var testing = options.getSeriesByType(Options.SERIES_TYPES.COLUMN);
+      var testing = options.getSeriesByType(SeriesOptions.TYPE.COLUMN);
       var expected = [];
 
       expect(testing).to.eql(expected);
