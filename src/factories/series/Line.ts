f57761906c069d9007c38ebd8b90431cb9fc47d9
Sébastien <sebastien@metamarkets.com>
2015-10-28 14:47:05 +0100

    Added dashed line type

--- src/factories/series/Line.ts
+++ src/factories/series/Line.ts
@@ -6,6 +6,7 @@ module n3Charts.Factory.Series {
     public type: string = Utils.SeriesOptions.TYPE.LINE;
 
     updateData(group: D3.Selection, series: Utils.SeriesOptions, index: number, numSeries: number) {
+      group.classed('dashed', series.isDashed());
 
       var xAxis = <Factory.Axis>this.factoryMgr.get('x-axis');
       var yAxis = <Factory.Axis>this.factoryMgr.get('y-axis');
@@ -49,7 +50,8 @@ module n3Charts.Factory.Series {
     styleSeries(group: D3.Selection) {
       group.style({
         'fill': 'none',
-        'stroke': (s: Utils.SeriesOptions) => s.color
+        'stroke': (s: Utils.SeriesOptions) => s.color,
+        'stroke-dasharray': (s: Utils.SeriesOptions) => s.isDashed() ? '10,3' : undefined
       });
     }
   }
