6150dbed750053dbab71f83ff336a796850e781e
lorem--ipsum <dolor_sit_amet@protonmail.ch>
2016-04-01 19:31:53 +0200

    Updated release script

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-VERSION=`cat package.json | grep version | sed 's/[",:]//g' | awk '{print $2}'`
+currentVersion=`cat package.json | grep version | sed 's/[",:]//g' | awk '{print $2}'`
 
 # Eventually we'll have a branch named v2, I guess ?
 if [ "$(git branch | grep \* | egrep -o '\w+')" != "master" ]; then
@@ -14,14 +14,22 @@ if [ -n "$(git status --porcelain)" ]; then
 fi
 
 if [ "$1" == "" ]; then
-  echo ""
-  echo "Missing version number, aborting !"
-  echo "Usage: ./release.sh VERSION"
-  echo "Current version: ${VERSION}"
-  echo ""
-  exit 1
+  a=( ${currentVersion//./ } )
+  ((a[2]++))
+
+  read -r -p "Do you want to release ${a[0]}.${a[1]}.${a[2]} ? [Y/n] " response
+  if [[ $response =~ ^([nN][oO]|[nN])$ ]]
+  then
+    echo 'Aborting, have a good day'
+    exit 0
+  else
+    newVersion="${a[0]}.${a[1]}.${a[2]}"
+  fi
+else
+  newVersion=$1
 fi
 
+echo "Releasing ${newVersion} (previous was ${currentVersion})"
 release-it $1
 
 rm -r build
